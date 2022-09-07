5a6e002111c2604bd32bf57b65454b4b8a83aba1
Hamza Altamas <hamza.altamas.01876@gmail.com>
2022-09-07 14:59:18 +0600

    The biggest supplier on the country part added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,17 @@ tailwind.config = {
         clifford: "#da373d",
         red: "#F40404",
         yellow: "rgb(250 204 21)",
+        bannerOverlay: "rgba(0, 0, 0, 0.6)",
+        paraColor: "#6C6C6C",
       },
-      spacing: {
+      maxWidth: {
         conatiner: "1120px",
       },
       fontFamily: {
         pop: "Poppins",
+      },
+      backgroundImage: {
+        banner: "url('BannerBg.png')",
       },
     },
   },
